c2b6c5b6be85af88346bca16b351460a1948acc9
Roman Frołow <rofrol@gmail.com>
2014-12-28 00:38:47 +0100

    from_str -> parse

--- solve-me-first/src/main.rs
+++ solve-me-first/src/main.rs
@@ -3,10 +3,10 @@ use std::io;
 fn main() {
     let line1: String = io::stdin().read_line().unwrap();
     let line1_trimmed: &str = line1.as_slice().trim();
-    let num1: int = from_str(line1_trimmed).unwrap();
+    let num1: int = line1_trimmed.parse::<int>().unwrap();
     let line2: String = io::stdin().read_line().unwrap();
     let line2_trimmed: &str = line2.as_slice().trim();
-    let num2: int = from_str(line2_trimmed).unwrap();
+    let num2: int = line2_trimmed.parse::<int>().unwrap();
     //println! adds new line
     //println!("{}", num1 + num2);
     //but print! doesn't print anything
