f1404bf897f3be3a7e68ab6ec57dcc0cbb65f04d
Roman Frołow <rofrol@gmail.com>
2014-12-29 09:34:53 +0100

    find digits

---
+++ src/bin/find-digits.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::io::BufferedReader;
+fn main() {
+    let line = io::stdin().read_line().unwrap();
+    let line_t: &str = line.trim();
+    let count: int = line_t.parse::<int>().unwrap();
+    let mut v: Vec<int> = Vec::new();
+    let mut reader = BufferedReader::new(io::stdin());
+    for line in reader.lines() {
+        let line_u: String = line.unwrap();
+        let line_t: &str = line_u.as_slice().trim();
+        let num: int = line_t.parse::<int>().unwrap();
+        v.push(num);
+    }
+    let mut k: int;
+    let mut count: int;
+    for x in v.iter() {
+        k = *x;
+        count = 0;
+        while k != 0 {
+            // don't divide by 0 && don't count k < 10, ie. 3%(3%10) == 0
+            if k % 10 != 0 && k >= 10 && k % (k % 10) == 0 {
+                count += 1;
+            }
+            k = k/10;
+        }
+        println!("{}", count);
+    }
+}
