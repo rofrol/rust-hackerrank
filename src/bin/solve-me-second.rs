1f9e1330f705b41c6744f15c3b38e44ba520ed8a
Roman Frołow <rofrol@gmail.com>
2015-01-15 22:28:53 +0100

    solve-me-second - read_line in one line

--- src/bin/solve-me-second.rs
+++ src/bin/solve-me-second.rs
@@ -3,10 +3,9 @@ fn main() {
     let lines: u32 = io::stdin().read_line().ok().unwrap().trim().parse().unwrap();
     let mut num: u64;
     for i in range(0u32, lines) {
-        let line_u = io::stdin().read_line().unwrap();
-        let line_t = line_u.as_slice().trim();
+        let line = io::stdin().read_line().ok().unwrap().trim().to_string();
         num = 0;
-        for item in line_t.as_slice().split(' ') {
+        for item in line.as_slice().split(' ') {
             num += item.parse::<u64>().unwrap();
         }
         println!("{}", num);
