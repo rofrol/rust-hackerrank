bf20c9bfae874fc4cf602db1f181d21751322b5d
Roman Frołow <rofrol@gmail.com>
2015-01-17 02:01:55 +0100

    cleaning

--- src/bin/find-digits.rs
+++ src/bin/find-digits.rs
@@ -1,22 +1,15 @@
 use std::io;
 fn main() {
-    let line_u = io::stdin().read_line().unwrap();
-    let line_t: &str = line_u.trim();
-    let lines: u64 = line_t.parse::<u64>().unwrap();
-    let mut v: Vec<u64> = Vec::new();
-    for i in range(0u64, lines) {
-        let line_u = io::stdin().read_line().unwrap();
-        let line_t: &str = line_u.as_slice().trim();
-        let num: u64 = line_t.parse::<u64>().unwrap();
-        v.push(num);
-    }
+    let lines: u32 = io::stdin().read_line().ok().unwrap().trim().parse().unwrap();
     let mut k: u64;
+    let mut num: u64;
     let mut count: u64;
-    for x in v.iter() {
-        k = *x;
+    for i in range(0u32, lines) {
+        num = io::stdin().read_line().ok().unwrap().trim().parse().unwrap();
+        k = num;
         count = 0;
         while k != 0 {
-            if k % 10 != 0 && *x % (k % 10) == 0 {
+            if k % 10 != 0 && num % (k % 10) == 0 {
                 count += 1;
             }
             k = k / 10;
